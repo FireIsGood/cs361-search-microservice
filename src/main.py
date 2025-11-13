a97940543d12e276c3d271e1781304f03dfa89c1
callahjo <146775739+callahjo@users.noreply.github.com>
2025-11-13 09:14:14 -0800

    Created search function
    Search function was implemented in accordance to the issues. Case sensitive/insensitive options are present while returning all possible results.

--- src/main.py
+++ src/main.py
@@ -21,6 +21,34 @@ def load_search_values():
             global search_values
             search_values = file.read().splitlines()
 
+@app.route("/search", methods=["GET"])
+def search():
+    data= request.args
+    query = request.args.get("query", "")
+    caseless_query = query.lower()
+    
+    case_option = data.get("caseSensitive", "false") # If none in URL, then the default is false
+    case_sensitive = case_option.lower() == "true" 
+    req_options = {
+        "caseSensitive":case_sensitive
+    }
+
+    results = []
+    for line in search_values:   #search Iteration
+        if case_sensitive:        # Case sensitivity true
+            if query in line:
+                results.append(line)
+        else:                    # Case sensitive false
+            if caseless_query in line.lower():
+                results.append(line)
+                
+    return {
+        "query": query,
+        "options": req_options,
+        "results": results
+    }
+    
+
 
 def main():
     try:
